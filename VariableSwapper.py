c1f2723c59eb03eef085b38dceb7475661630c2c
D8M <davidmahon0000@gmail.com>
2023-03-10 12:35:12 +0000

    Day 1 Lesson 14 -Asks user to input for two vars and swap the values after

---
+++ VariableSwapper.py
@@ -0,0 +1,14 @@
+a, b = input("\nPlease enter the two values for a and b: ").split()
+
+print("\na is",a + " and b is", b)
+
+a,b = b,a  # Using standard py way to swap id's on two objects (not variables ;)
+
+
+print("\nAfter swapping: a is",a + " and b is", b)  # Py evaluates the rh side of the equation 1st, then the lh side
+
+
+
+
+
+
