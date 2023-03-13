9c8bfb6406e01765801e2447c45a46262482e749
D8M <davidmahon0000@gmail.com>
2023-03-13 22:43:42 +0000

    Lesson 30 - Odd or Even using Modulo. A program to work out whether a user typed number is odd or even. Updated with FORE colour system and some textual changes.

---
+++ OddOrEven.py
@@ -0,0 +1,15 @@
+import colorama
+from colorama import Fore
+from colorama import Style
+
+print(f"{Fore.GREEN}**********************************************{Style.RESET_ALL}")
+print(f"{Fore.GREEN}*             Odd or Even number             *{Style.RESET_ALL}")
+print(f"{Fore.GREEN}**********************************************\n{Style.RESET_ALL}")
+
+numToCheck = int(input ("Please enter a number to check and see if its odd or even: "))
+
+if numToCheck % 2 == 0:
+    print(numToCheck,"number is even")
+
+else:
+    print(numToCheck, "number is odd")
