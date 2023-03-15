a7905165db00ee9a849d55865d1199604ec2f69d
D8M <davidmahon0000@gmail.com>
2023-03-15 11:36:41 +0000

    Lesson 32 - More advanced BMI checker with printed advice output per chosen weight/height

--- Lesson-23 BMI Calculator.py
+++ Lesson-23 BMI Calculator.py
@@ -9,3 +9,18 @@ BMI = float(yourWeight)/(float(yourHeight) ** 2)
 
 print("Your BMI is: ",round(BMI,2))
 
+if BMI <= 18.5:
+    print(" You may be underweight\n")
+elif 18.5 < BMI < 25:
+    print("You are in the normal weight zone.\n")
+elif 25 < BMI <= 30:
+    print(" You may be overweight\n")
+elif 30 < BMI > 35:
+    print(" You may be obese\n")
+else:
+    print(" You may be clinically obese\n")
+
+
+
+
+
