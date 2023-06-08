339ff836ff43a6dd94c3fb240fa79c18c94de458
D8M <davidmahon0000@gmail.com>
2023-06-08 14:31:34 +0100

    Lesson 62 - While loops py

---
+++ Lesson 62 - While Loops.py
@@ -0,0 +1,12 @@
+import random
+import streamlit as st
+from collections import Counter
+from colorama import Fore
+from colorama import Style
+
+print(f"{Fore.RED}**********************************************{Style.RESET_ALL}")
+print(f"{Fore.GREEN}*         Py While Loops                     *{Style.RESET_ALL}")
+print(f"{Fore.YELLOW}**********************************************\n{Style.RESET_ALL}")
+
+print("Welcome to the Py While Loops.\n")
+
