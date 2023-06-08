8c0f071b491cc661957344a0ce94010cc0ff3e86
D8M <davidmahon0000@gmail.com>
2023-06-08 14:31:59 +0100

    Lesson 59 - Functions def & call

---
+++ Lessonn 59 - Functions Def & Call.py
@@ -0,0 +1,15 @@
+import random
+from collections import Counter
+from colorama import Fore
+from colorama import Style
+
+print(f"{Fore.RED}**********************************************{Style.RESET_ALL}")
+print(f"{Fore.GREEN}*         Py Lesson 59 - Functions           *{Style.RESET_ALL}")
+print(f"{Fore.YELLOW}**********************************************\n{Style.RESET_ALL}")
+
+print("Welcome to the Py Functions.\n")
+
+# Create or Define a function by using def function_name(): ->def ->name ->brackets -> colon
+
+
+ 
